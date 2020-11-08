f228d1ff2d0248c80351527b94c67ab79e74bf25
Ignacio Lopez Scala <49875658+Ignacioals@users.noreply.github.com>
2020-11-08 03:07:09 -0300

    Project init

---
+++ App.js
@@ -0,0 +1,131 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { Platform, StatusBar, Image, Text, StyleSheet, View, TextInput, Button } from "react-native";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Block, GalioProvider } from "galio-framework";
+
+import { Images, products, materialTheme } from "./constants/";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+
+
+
+import * as BackgroundFetch from "expo-background-fetch";
+import * as TaskManager from "expo-task-manager";
+import { Formik } from 'formik';
+import LogIn from './screens/LogIn';
+// Before rendering any navigation stack
+import { enableScreens } from "react-native-screens";
+
+
+enableScreens();
+
+// cache app images
+const assetImages = [
+  Images.Pro,
+  Images.Profile,
+  Images.Avatar,
+  Images.Onboarding,
+];
+
+// cache product images
+products.map((product) => assetImages.push(product.image));
+
+const TASK_NAME = "BACKGROUND_TASK";
+
+TaskManager.defineTask(TASK_NAME, () => {
+  try {
+    // fetch data here...
+    const receivedNewData = "Simulated fetch " + Math.random();
+    console.log("My task ", receivedNewData);
+
+    //TODO: identificar vencimientos y registrarlos en otra tabla
+    //TODO: registrar movimientos en cuenta para ingresos recurrentes y debitos automaticos
+    //TODO: actualizar saldos de las cuentas bancarias
+    return receivedNewData
+      ? BackgroundFetch.Result.NewData
+      : BackgroundFetch.Result.NoData;
+  } catch (err) {
+    return BackgroundFetch.Result.Failed;
+  }
+});
+
+const registerBackgroundTask = async () => {
+  try {
+    await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+      minimumInterval: 10, // seconds,
+    });
+    console.log("Task registered");
+  } catch (err) {
+    console.log("Task Register failed:", err);
+  }
+};
+
+function cacheImages(images) {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+
+
+export default function App(props) {
+
+  useEffect(() => {
+    registerBackgroundTask();
+  }, []);
+
+  const [isLoadingComplete, setIsLoadingComplete] = useState(false);
+
+  const _loadResourcesAsync = async () => {
+    const assetFont = Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font,
+    });
+    return Promise.all([assetFont, ...cacheImages(assetImages)]);
+  };
+
+  const _handleLoadingError = (error) => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  const _handleFinishLoading = () => {
+    setIsLoadingComplete(true);
+  };
+
+
+
+  return (
+    <React.Fragment >
+        {!isLoadingComplete && !props.skipLoadingScreen && (
+          <AppLoading
+            startAsync={_loadResourcesAsync}
+            onError={_handleLoadingError}
+            onFinish={_handleFinishLoading}
+          />
+        )}
+        {isLoadingComplete && (
+          <NavigationContainer>
+            <GalioProvider theme={materialTheme}>
+              <Block flex>
+                {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+                <LogIn/>
+              </Block>
+            </GalioProvider>
+          </NavigationContainer>
+        )}
+      
+
+    </React.Fragment> 
+  );
+}
---
+++ screens/LogIn.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import { ImageBackground, StyleSheet, StatusBar, Dimensions, Platform , View, TextInput} from 'react-native';
+import { Block, Button, Text, theme } from 'galio-framework';
+
+const { height, width } = Dimensions.get('screen');
+import * as yup from 'yup';
+import { Formik } from 'formik';
+
+import materialTheme from '../constants/Theme';
+import Images from '../constants/Images';
+import Logo from '../assets/images/LogoBankMe.png';
+import { TouchableOpacity } from 'react-native';
+
+export default class LogIn extends React.Component {
+  render() {
+    const { navigation } = this.props;
+  // validaciones de login
+    const loginValidationSchema = yup.object().shape({
+      // email: yup
+      //   .string()
+      //   .email("Please enter valid email")
+      //   .required('Email Address is Required'),
+      password: yup
+        .string()
+        .min(8, ({ min }) => `La contraseña debe ser de al menos ${min} caracteres`)
+        .required('Contraseña necesaria'),
+    })  
+    return (
+      <Block flex style={{backgroundColor: materialTheme.COLORS.BACKGROUND, width:width, height:height}}>
+        {/* <StatusBar barStyle="light-content" /> */}
+        <Block flex center>
+          <ImageBackground
+            source={Logo}
+            style={{ height: "90%", width: "90%", marginLeft: '-85%', marginTop:"10%", zIndex: 1 }}
+          />
+        </Block>
+        <Block flex space="between" style={{top:"-20%"}}>
+          <Block flex space="around" style={{ zIndex: 2 }}>
+              <Block>
+                <Text color="white" size={40} style={{padding:"10%"}}>Hola, Bienvenid@!</Text>
+              </Block>
+          </Block>
+        </Block>
+        {/* <Text
+            style={{color: materialTheme.COLORS.BUTTON_COLOR, textDecorationLine: 'underline', top:"80%"}}
+            
+            //aca despues le pongo el onPress a la pag de registro
+        >
+          {"¿"}No te registraste todavia{"?"}
+        </Text> */}
+        <View style={styles.loginContainer}>
+          <Formik
+            enableReinitialize
+            validationSchema={loginValidationSchema}
+            initialValues={{ usuario: '', password: '' }}
+            onSubmit={values => console.log(values)}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              touched,
+              isValid,
+            }) => (
+              <>
+                <TextInput
+                  name="usuario"
+                  placeholder="  Usuario"
+                  style={styles.textInput}
+                  onChangeText={handleChange('usuario')}
+                  onBlur={handleBlur('usuario')}
+                  value={values.usuario}
+                  keyboardType= "default"
+                />
+                <TextInput
+                  name="password"
+                  placeholder=" Contraseña"
+                  style={styles.textInput}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  keyboardType= "ascii-capable"
+                  secureTextEntry
+                />
+                {(touched.password && errors.password ) &&
+                                  <Text style={styles.errorText}>{errors.password}</Text>
+                }   
+                <View style={{}}>
+                  <Button
+                    onPress={handleSubmit}
+                    color= {materialTheme.COLORS.BUTTON_COLOR}
+                    title="Ingresar"
+                    disabled={!isValid}
+                    style={styles.button}
+                  >
+                    <TouchableOpacity                  onPress={handleSubmit}
+                      color= {materialTheme.COLORS.BUTTON_COLOR}
+                      // title="Ingresar"
+                      disabled={!isValid}
+                      style={styles.button}>
+                        <Text>Ingresar </Text>
+                    </TouchableOpacity>
+                  </Button>
+                </View>
+              </>
+            )}
+          </Formik>
+        <View style={{top:"30%"}}>
+          <Text
+            style={{color: materialTheme.COLORS.BUTTON_COLOR, textDecorationLine: 'underline', top:"25%"}}
+            onPress={() => props.navigation.navigate("Registrar")}
+            //aca despues le pongo el onPress a la pag de registro
+          >
+          {"¿"}Olvidaste tu contraseña{"?"} 
+          </Text>
+          <Text
+            style={{color: materialTheme.COLORS.BUTTON_COLOR, textDecorationLine: 'underline', top:"50%"}}
+            // onPress={}
+
+            //aca despues le pongo el onPress a la pag de registro
+          >
+          {"¿"}Primera vez que ingresas{"?"}
+          </Text>
+        </View>
+      </View>
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    
+    // width: width - theme.SIZES.BASE * 3,
+    // height: theme.SIZES.BASE * 3,
+    shadowRadius: 0,
+    shadowOpacity: 0,
+  },
+  loginContainer: {
+    width: '80%',
+    alignItems: 'center',
+    padding: 10,
+    elevation: 10,
+    backgroundColor: '#ffbd59',
+    top:"-20%",
+    marginLeft:"10%"
+  },
+  textInput: {
+    height: 40,
+    width: '100%',
+    margin: 10,
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    // borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+  },
+  errorText: {
+    fontSize: 10,
+    color: 'red',
+  },
+});
